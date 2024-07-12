7b8f07385e57d3a73c7a1858684340f34df8f8bd
yeonee <155801470+y-eonee@users.noreply.github.com>
2024-07-12 16:39:01 +0900

    폴더 정리

--- Front/js/background.js
+++
@@ -1,8 +0,0 @@
-const images = ["img1.jpg", "img2.jpg", "img3.jpg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
-console.log(bgImage.src);
-document.body.appendChild(bgImage);
