c091694d0584d65c20785b272887471ef334c0b8
yeonee <155801470+y-eonee@users.noreply.github.com>
2024-08-03 03:33:08 +0900

    Update Home.js

--- Front/ReactJS_Movie_Lecture/Section7_Challenge/src/routes/Home.js
+++ Front/ReactJS_Movie_Lecture/Section7_Challenge/src/routes/Home.js
@@ -2,16 +2,16 @@ import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
 
 function Home(){
-    const [loading, setLoading] = useState(true); 
-    const [movies, setMovies] = useState([]);
-    const getMovies = async() =>{
+    const [loading, setLoading] = useState(true); //loading state
+    const [movies, setMovies] = useState([]); //movie list state
+    const getMovies = async() =>{ //movie list api 접근 
         const response = await fetch(
         `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
         );
 
         const json = await response.json();
         setMovies(json.data.movies);
-        setLoading(false);
+        setLoading(false); //loading 완료
     }
     useEffect(()=>{
         getMovies();
@@ -20,7 +20,7 @@ function Home(){
     return (
         <div>
         {loading ? <h1>Loading...</h1> : movies.map(movie => 
-            <Movie 
+            <Movie //movie 컴포넌트에 props 넘겨줌 
             key = {movie.id}
             id= {movie.id}
             coverImg = {movie.medium_cover_image} 
